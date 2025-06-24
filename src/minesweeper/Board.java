046569e1081d28729e98aa8de65bea8210be6977
Dunya Hamidi <dh581@kent.ac.uk>
2025-06-24 23:35:24 +0100

    Board class imports

--- src/minesweeper/Board.java
+++ src/minesweeper/Board.java
@@ -1,4 +1,10 @@
 package minesweeper;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import static minesweeper.Constants.ROWS;
+import static minesweeper.Constants.COLUMNS;
 public class Board {
+
 }
--- src/minesweeper/Map.java
+++ src/minesweeper/Map.java
@@ -5,11 +5,25 @@ import static minesweeper.Constants.COLUMNS;
 
 /** This class contains the locations of the mines. */
 public class Map {
-int numMines;
-boolean[][] isMined = new boolean[ROWS][COLUMNS];
+    int numMines;
+    boolean[][] isMined = new boolean[ROWS][COLUMNS];
 
-public Map() {
-    super();
-}
+    public Map() {
+        super();
+    }
 
+    public void newMap (int numMines) {
+        this.numMines = numMines;
+        // Hardcoded (temporarily)
+        isMined[0][0] = true;
+        isMined[5][2] = true;
+        isMined[9][5] = true;
+        isMined[6][7] = true;
+        isMined[8][2] = true;
+        isMined[2][4] = true;
+        isMined[5][7] = true;
+        isMined[7][7] = true;
+        isMined[3][6] = true;
+        isMined[4][8] = true;
+    }
 }
