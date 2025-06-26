c95867745305cb9f48e50da133cdf6265f87bd7f
Dunya Hamidi <dh581@kent.ac.uk>
2025-06-26 22:47:21 +0100

    Main class

--- src/minesweeper/Board.java
+++ src/minesweeper/Board.java
@@ -31,6 +31,59 @@ public class Board extends JPanel {
             }
         }
 
-        
+        // 
+
+        //
+
+        // Set size of content-pane and pack all components under container. 
+        super.setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
     }
+
+    // Initialize the game board with a new game.
+    public void newGame() {
+        Map mineMap = new Map();
+        mineMap.newMap(numMines);
+
+        for (int row = 0; row < ROWS; row++) {
+            for (int column = 0; column < COLUMNS; column++) {
+                cells[row][column].newGame(mineMap.isMined[row][column]);
+            }
+        }
+    }
+
+    // Return the number of mines in the neighbouring cells.
+    private int getSurroundingMines(int srcRow, int srcCol) {
+        int numMines = 0;
+        for (int row = srcRow - 1; row <= srcRow + 1; row++) {
+            for (int col = srcCol - 1; col <= srcCol + 1; col++) {
+               // Need to ensure valid row and column numbers too
+               if (row >= 0 && row < ROWS && col >= 0 && col < COLUMNS) {
+                  if (cells[row][col].isMined) numMines++;
+               }
+            }
+        }
+        return numMines;  
+    }
+
+    //
+    private void revealCell(int srcRow, int srcCol) {
+        int numMines = getSurroundingMines(srcRow, srcCol);
+        cells[srcRow][srcCol].setText(numMines + "");
+        cells[srcRow][srcCol].isRevealed = true;
+        cells[srcRow][srcCol].paint();
+        if (numMines == 0) {
+            for (int row = srcRow - 1; row <= srcRow + 1; row++) {
+                for (int col = srcCol - 1; col <= srcCol + 1; col++) {
+                    // Need to ensure valid row and column numbers too
+                    if (row >= 0 && row < ROWS && col >= 0 && col < COLUMNS) {
+                        if (!cells[row][col].isRevealed) revealCell(row, col);
+                    }
+                }
+            }
+        }
+    }
+
+    // Return true if the player has won
+
+    // Define a Listener Inner class
 }
--- src/minesweeper/Main.java
+++ src/minesweeper/Main.java
@@ -1,4 +1,36 @@
 package minesweeper;
 
-public class Main {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/*
+ * 
+ */
+public class Main extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    Board board = new Board();
+    JButton newGameButton = new JButton("New Game");
+
+    // Constructor
+    public Main() {
+        Container cp = this.getContentPane();
+        cp.setLayout(new BorderLayout());
+        cp.add(board, BorderLayout.CENTER);
+
+        //
+
+        board.newGame();
+
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Minesweeper");
+        setVisible(true);
+    }
+
+    public static void main (String[] args) {
+        //
+    }
 }
+
